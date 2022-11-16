ae7fe8029b23f2ce3f80351d88b62ebdd2773f19
Erick Alonso <erick.edjaa@gmail.com>
2022-11-15 22:28:52 -0600

    Se genero el pdf con exito de la informacion traida desde la base de datos

--- src/app/components/ine-report-form/ine-report-form.component.ts
+++ src/app/components/ine-report-form/ine-report-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PdfMakeWrapper,Txt } from 'pdfmake-wrapper';
 import { PersonService } from 'src/app/services/person.service';
+import { UserService } from 'src/app/services/user.service';
 import { Person } from 'src/app/models/person';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-ine-report-form',
@@ -10,7 +12,15 @@ import { Person } from 'src/app/models/person';
 })
 export class IneReportFormComponent implements OnInit {
 
-  constructor() { }
+  public persona:Person;
+  public dataPerson:any;
+
+  constructor(
+    private userService:UserService,
+    private personService:PersonService
+  ) {
+    this.persona = new Person(1,1,'','','','','',1,'','','','',1,'','','');
+   }
 
   ngOnInit(): void {
   }
@@ -23,6 +33,31 @@ export class IneReportFormComponent implements OnInit {
     );
 
     pdf.create().open();
+  }
+
+  public getIne(ine){
+    this.personService.reporte_ine(ine).subscribe(
+      (response) => {
+        if(response.status == 'success'){
+          const pdf = new PdfMakeWrapper();
+          pdf.add(
+            new Txt(response.person.name).alignment('center').bold().italics().end,
+          );
+
+          pdf.create().open();
+          
+        }else{
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: "No se logro Generar el reporte"
+          })
+        }
+      },
+      (error) => {
+        console.error(<any>error);
+      }
+    );
   }
   
 
