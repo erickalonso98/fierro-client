9b26ec9db9eb5de3c448ac712f8753b22803df54
Erick Alonso <erick.edjaa@gmail.com>
2022-11-18 00:29:59 -0600

    se obtuvo el detalle del fierro en el componente iron-detail

--- src/app/components/iron-detail/iron-detail.component.ts
+++ src/app/components/iron-detail/iron-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute,Params } from '@angular/router';
+import { IronService } from 'src/app/services/iron.service';
+import { Iron } from 'src/app/models/iron';
+import { global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-iron-detail',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IronDetailComponent implements OnInit {
 
-  constructor() { }
+  public url:string;
+  public iron:Iron;
+  public page_title:string;
+
+  constructor(
+    private _ironService:IronService,
+    private _route:ActivatedRoute,
+    private _router:Router
+  ) { 
+    this.page_title = 'Detalle del Fierro';
+    this.url = global.url;
+  }
 
   ngOnInit(): void {
+    this.getIron();
+  }
+
+  public getIron(){
+    this._route.params.subscribe((params) => {
+      let id = +params['id'];
+      this._ironService.getIron(id).subscribe(
+        (response) => {
+          if(response.status == 'success'){
+            this.iron = response.iron;
+            console.log(this.iron);
+          }else{
+            this._router.navigate(['/iron']);
+          }
+        },
+        (error) => {
+          console.error(<any>error);
+        }
+      );
+
+    });
   }
 
 }
