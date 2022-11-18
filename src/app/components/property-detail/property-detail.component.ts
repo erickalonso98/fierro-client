36d22f0279a1ecfa7f46d97ea67caa5b8f8478c7
Erick Alonso <erick.edjaa@gmail.com>
2022-11-18 01:21:46 -0600

    se probo el componente detalle del predio y se muestra los datos correctamente

--- src/app/components/property-detail/property-detail.component.ts
+++ src/app/components/property-detail/property-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute,Params } from '@angular/router';
+import { PropertyService } from 'src/app/services/property.service';
+import { Property } from 'src/app/models/property';
+import { global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-property-detail',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyDetailComponent implements OnInit {
 
-  constructor() { }
+  public page_title:string;
+  public url:string;
+  public property:Property;
+
+  constructor(
+    private _propertyService:PropertyService,
+    private _route:ActivatedRoute,
+    private _router:Router
+  ) {
+    this.url = global.url;
+    this.page_title = 'Detalle del predio ganadero';
+   }
 
   ngOnInit(): void {
+    this.getProperty();
+  }
+
+  public getProperty(){
+    this._route.params.subscribe((params) => {
+      let id = +params['id'];
+      this._propertyService.getProperty(id).subscribe(
+        (response) => {
+          if(response.status == 'success'){
+            this.property = response.property;
+            console.log(this.property);
+          }else{
+            this._router.navigate(['/property']);
+          }
+        },
+        (error) => {
+          console.error(<any>error);
+        }
+      );
+    });
   }
 
 }
