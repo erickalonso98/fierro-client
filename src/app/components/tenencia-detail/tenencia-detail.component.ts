d74aa00fe91d25b42d6880d6788852471527ad1b
Erick Alonso <erick.edjaa@gmail.com>
2022-11-18 23:29:52 -0600

    se modifico el componente tenencia-detail.ts para mostrar los datos por id

--- src/app/components/tenencia-detail/tenencia-detail.component.ts
+++ src/app/components/tenencia-detail/tenencia-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute,Params } from '@angular/router';
+import { TenenciaService } from 'src/app/services/tenencia.service';
+import { Tenencia } from 'src/app/models/tenencia';
 
 @Component({
   selector: 'app-tenencia-detail',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TenenciaDetailComponent implements OnInit {
 
-  constructor() { }
+  public tenencia:Tenencia;
+  constructor(
+    private _tenenciaService:TenenciaService,
+    private _route:ActivatedRoute,
+    private _router:Router
+  ) { }
 
   ngOnInit(): void {
+    this.getTenenciaOne();
+  }
+
+  public getTenenciaOne(){
+    this._route.params.subscribe((params) => {
+      let id = +params['id'];
+      this._tenenciaService.getTenencia(id).subscribe(
+        (response) => {
+          if(response.status == 'success'){
+            this.tenencia = response.tenencia;
+            console.log(this.tenencia);
+          }else{
+            this._router.navigate(['/type-tenencia']);
+          }
+        },
+        (error) => {
+          console.error(<any>error);
+        }
+      );
+    });
   }
 
 }
