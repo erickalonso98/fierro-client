2c7aa6e69b0c0562280c512628596cc8cc1e0235
Erick Alonso <erick.edjaa@gmail.com>
2022-11-18 23:46:17 -0600

    se agregaron nuevas librerias para el componente iron-detail.ts

--- src/app/components/type-iron-detail/type-iron-detail.component.ts
+++ src/app/components/type-iron-detail/type-iron-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute,Params } from '@angular/router';
+import { IronTypeService } from 'src/app/services/iron-type.service';
+import { Iron_Type } from 'src/app/models/iron_type';
 
 @Component({
   selector: 'app-type-iron-detail',
