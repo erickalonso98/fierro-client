d300beeb4594dbb6d5332a8488fef5df250a4983
Erick Alonso <erick.edjaa@gmail.com>
2022-11-23 20:47:40 -0600

    se creo unos metodos en el user.service para obtener el id del usuario

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute,Params } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { global } from 'src/app/services/global';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  public page_title:string;
+  public user:User;
+  public url:string;
+  public identity:any;
+  public token:any;
+
+  constructor(
+    private _userService:UserService,
+    private _route:ActivatedRoute,
+    private _router:Router
+  ) {
+    this.page_title = 'Detalle de perfil del usuario';
+    this.url = global.url;
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+   }
 
   ngOnInit(): void {
+    this.getProfile();
+  }
+
+  public getProfile(){
+    this._route.params.subscribe((params)=>{
+      let userId = +params['id'];
+      this.getUser(userId);
+    });
+  }
+
+  public getUser(userId){
+    this._userService.profile(userId).subscribe(
+      (response) => {
+        if(response.status == 'success'){
+          this.user = response.user;
+          console.log(this.user);
+        }
+      },
+      (error) => {
+        console.error(<any>error);
+      }
+    );
   }
 
 }
