c60b2074f6ab1c3ad0cd52316b6aca0d4e9d3fc6
Erick Alonso <erick.edjaa@gmail.com>
2022-12-03 22:49:33 -0600

    se agregaron nuevas listas de navegacion de la interfaces name,rfc

--- src/app/components/report-name/report-name.component.ts
+++ src/app/components/report-name/report-name.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportNameComponent implements OnInit {
 
+  public opened:boolean = false;
+  public open:string = "Abrir Menu";
+  public menu:string = "Menú";
+  public cerrar:string = "Cerrar";
+  public person:string = "Personas";
+  public property:string = "Predial";
+  public iron:string = "Fierro";
+  public exploitation:string = "Tipo de explotación";
+  public tenencia:string = "Tipo de tenencia de la tierra";
+  public type_iron:string = "Tipo de fierro";
+  public users:string = "Usuarios";
+  public roles:string = "Roles de usuarios";
+  public report:string = "Reportes";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public toggleSidebar() {
+    this.opened = !this.opened;
   }
 
 }
