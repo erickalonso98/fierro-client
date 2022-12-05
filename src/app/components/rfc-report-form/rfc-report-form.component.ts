3067ee3c98b183f2a1fdec897cb6b30e850973bd
Erick Alonso <erick.edjaa@gmail.com>
2022-12-04 21:44:20 -0600

    se obtuvo la rfc con exito

--- src/app/components/rfc-report-form/rfc-report-form.component.ts
+++ src/app/components/rfc-report-form/rfc-report-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { PersonService } from 'src/app/services/person.service';
+import { Person } from 'src/app/models/person';
+import { global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-rfc-report-form',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RfcReportFormComponent implements OnInit {
 
-  constructor() { }
+  public page_title:string;
+  public persona:Person;
+  public url:string;
+
+  constructor(
+    private _userService:UserService,
+    private _personService:PersonService
+  ) { 
+    this.page_title = 'Reporte x rfc';
+    this.persona = new Person(1,1,'','','','','',1,'','','','',1,'','','');
+    this.url = global.url;
+  }
 
   ngOnInit(): void {
+  }
+
+  public getRfc(rfc){
+    this._personService.reporte_rfc(rfc).subscribe(
+      (response) => {
+        if(response.status == 'success'){
+          console.log(response);
+        }
+      },
+      (error) => {
+        console.error(<any>error);
+      }
+    );
   }
 
 }
