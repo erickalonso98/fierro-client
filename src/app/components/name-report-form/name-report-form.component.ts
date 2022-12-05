fe9ce32b29c8037bebec89c5f83978cddde4297a
Erick Alonso <erick.edjaa@gmail.com>
2022-12-04 22:06:14 -0600

    se obtuvo la informacion de la persona mediante su nombre con exito

--- src/app/components/name-report-form/name-report-form.component.ts
+++ src/app/components/name-report-form/name-report-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonService } from 'src/app/services/person.service';
+import { Person } from 'src/app/models/person';
+import { global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-name-report-form',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NameReportFormComponent implements OnInit {
 
-  constructor() { }
+  public page_title:string;
+  public persona:Person;
+  public url:string;
+
+  constructor(
+    private _personService:PersonService
+  ) { 
+    this.page_title = 'Reporte x nombre';
+    this.persona = new Person(1,1,'','','','','',1,'','','','',1,'','','');
+    this.url = global.url;
+  }
 
   ngOnInit(): void {
+  }
+
+  public getName(name){
+    this._personService.reporte_name(name).subscribe(
+      (response) => {
+        if(response.status == 'success'){
+          console.log(response);
+        }
+      },
+      (error) => {
+        console.error(<any>error);
+      }
+    );
   }
 
 }
