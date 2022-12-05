b1f18f74e41d7241c8f65c297d4cd3208fc573e5
Erick Alonso <erick.edjaa@gmail.com>
2022-12-04 21:22:03 -0600

    se obtuvo la informacion de la persona x curp correctamente

--- src/app/components/curp-report-form/curp-report-form.component.ts
+++ src/app/components/curp-report-form/curp-report-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonService } from 'src/app/services/person.service';
+import { UserService } from 'src/app/services/user.service';
+import { Person } from 'src/app/models/person';
+import { global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-curp-report-form',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurpReportFormComponent implements OnInit {
 
-  constructor() { }
+  public url:string;
+  public page_title:string;
+  public persona:Person;
+
+  constructor(
+    private _userService:UserService,
+    private _personService:PersonService
+  ) {
+    this.page_title = 'Reporte x Curp';
+    this.url = global.url;
+    this.persona = new Person(1,1,'','','','','',1,'','','','',1,'','','');
+   }
 
   ngOnInit(): void {
+  }
+
+  public getCurp(curp){
+    this._personService.reporte_curp(curp).subscribe(
+      (response) => {
+        if(response.status == 'success'){
+          console.log(response);
+        }
+      },
+      (error) => {
+        console.error(<any>error);
+      }
+    );
   }
 
 }
