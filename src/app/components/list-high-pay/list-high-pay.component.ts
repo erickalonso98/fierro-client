b50181d6a60de65711177c762367e34937a88ae5
Erick Alonso <erick.edjaa@gmail.com>
2022-12-29 00:25:36 -0600

    se crearon nuevos componentes para mostrar el alta de folio de fierro

---
+++ src/app/components/list-high-pay/list-high-pay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-high-pay',
+  templateUrl: './list-high-pay.component.html',
+  styleUrls: ['./list-high-pay.component.css']
+})
+export class ListHighPayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/pay-high/pay-high.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pay-high',
+  templateUrl: './pay-high.component.html',
+  styleUrls: ['./pay-high.component.css']
+})
+export class PayHighComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
